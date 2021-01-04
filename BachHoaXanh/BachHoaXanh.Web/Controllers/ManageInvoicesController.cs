d7deb32539de0dd855161d6999ee43ed4f61d846
linhtp2000 <73704873+linhtp2000@users.noreply.github.com>
2021-01-04 12:12:45 +0700

    update

---
+++ BachHoaXanh/BachHoaXanh.Web/Controllers/ManageInvoicesController.cs
@@ -0,0 +1,28 @@
+﻿using BachHoaXanh.Data.Models;
+using BachHoaXanh.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BachHoaXanh.Web.Controllers
+{
+    public class ManageInvoicesController : Controller
+    {
+        BachHoaXanhDbContext bhx = new BachHoaXanhDbContext();
+        // GET: NhapHang
+        [HttpGet]
+        public ActionResult NhapHang()
+        {
+            ViewBag.NCC = bhx.Providers;
+            ViewBag.SP = bhx.Products;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NhapHang(IEnumerable<DetailsOfInvoice> Model)
+        {
+            return View();
+        }
+    }
+}
